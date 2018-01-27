ee615865d0eb095358dae7f2796e29d2433fd10e
Mike Rovers <mikerovers97@gmail.com>
2018-01-28 00:49:29 +0100

    Added sepered lookup file

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-let config = require('config');
-const _ = require('lodash');
-const Table = require('cli-table2');
+let config = require('config'); 
 let usbScanner = require('node-usb-barcode-scanner').usbScanner;
 let getDevices = require('node-usb-barcode-scanner').getDevices;
 const commandLineArgs = require('command-line-args')
+const Pusher = require('pusher');
+const lookup = require('./lookup');
 
-const optionDefinitions = [
-    { name: "lookup", alias: "l", type: Boolean, defaultOption: false }
-]
-
-const options = commandLineArgs(optionDefinitions);
+const options = commandLineArgs({
+    name: "lookup", 
+    alias: "l", 
+    type: Boolean, 
+    defaultOption: false 
+});
 
 if(options.lookup) {
     lookup();
 }
 
-function lookup() {
-    let connectedDevices = getDevices();
+let pusher = new Pusher({
+    appId: '464305',
+    key: 'c08a48edcfcca1cbafea',
+    secret: '08d7199560cde013b032',
+    cluster: 'eu',
+    encrypted: true
+});
 
-    let table = new Table({
-        head: ['Vendor ID', 'Productname'],
-        wordWrap: true
+try {   
+    let scanner = new usbScanner({
+        vendorId: config.get('Common.vendor_id'),
+        hidMap: {30: '1', 31: '2', 32: '3', 33: '4',34: '5',35: '6', 36: '7', 37: '8', 38: '9',39: '0',40: 'e'}
     });
-
-    _.map(getDevices(), (device) => {
-        table.push([device.vendorId, device.product]);
+    scanner.on("data", function(code){
+        console.log("recieved code : " + code);
+        pusher.trigger('my-channel', 'my-event', {
+            'message': code
+        });
     });
-
-    console.log('Available HID devices:\n' + 
-        table.toString()
-    );
-
-    process.exit();
+} catch(e) {
+    console.log(`The scanner with vendorId ${config.get('Common.vendor_id')} could not be found.\nTry running the program as sudo.`);
 }
-
-console.log(config.get('Common.vendor_id'));
-
-let scanner = new usbScanner({
-    vendorId:1155,
-    hidMap: {30: '1', 31: '2', 32: '3', 33: '4',34: '5',35: '6', 36: '7', 37: '8', 38: '9',39: '0',40: 'e'}
-});
-scanner.on("data", function(code){
-    console.log("recieved code : " + code);
-});
